302ea5576222cebabd30f919e7abe1ede50cd88c
Davide Imola <davide.imola@icloud.com>
2019-12-14 01:49:43 +0100

    Fixing form

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -5,6 +5,7 @@ import style from './Button.module.css';
 const button = (props) => (
     <button
         onClick={props.clicked}
+        disabled={props.disabled}
         className={[style.Button, style[props.btnType]].join(' ')}
     >
         {props.children}
